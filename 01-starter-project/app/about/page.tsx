d57ee3897a785a1780517c2382ed4263b8b9b571
xauroxs <xauroxs@gmail.com>
2023-11-02 18:14:01 +0300

    create about page, layout & error, update app layout, add loading

---
+++ 01-starter-project/app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About project</h1>
+      <p>This is a Next.js project, created for study purposes.</p>
+      <Link href="/">Home page</Link>
+    </div>
+  );
+}
